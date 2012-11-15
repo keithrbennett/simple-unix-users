071ea3196788aabfbe3977fe9128644cb116d0df
Keith Bennett <keithrbennett@gmail.com>
2012-11-15 17:01:02 -0500

    Added warning for Mac users that passwd file reading is not useful.

--- lib/simple-unix-users.rb
+++ lib/simple-unix-users.rb
@@ -1,5 +1,21 @@
 require "simple-unix-users/version"
 
+if /darwin/ === RUBY_PLATFORM
+  $stderr.puts "
+
+A message from the simple-unix-users gem...
+
+Although simple-unix-users will read a passwd file on Mac OS,
+it is not guaranteed to work correctly.  In addition, according to the Mac's
+/etc/passwd file:
+
+# Note that this file is consulted directly only when the system is running
+# in single-user mode.  At other times this information is provided by
+# Open Directory.
+
+"
+end
+
 module SimpleUnixUsers
   require_relative 'simple-unix-users/user'
   require_relative 'simple-unix-users/users'
