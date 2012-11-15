e0b366349dd4b222ff31cfbf36d315349b263c12
Keith R. Bennett <keithrbennett@gmail.com>
2012-11-15 15:00:08 -0500

    Added spec files.

---
+++ spec/simple-unix-users/user_spec.rb
@@ -0,0 +1,23 @@
+require 'rspec'
+
+require_relative '../../lib/ruby-sysadmin-examples/user'
+
+describe User do
+
+subject { User.new( 'kbennett:x:1000:1000:Keith Bennett,,,:/home/kbennett:/usr/bin/zsh') }
+
+it 'should create correctly from the passwd line' do
+  subject.class.should == User
+end
+
+it 'should create fields correctly' do
+  subject.name.should == 'kbennett'
+  subject.uid.should == '1000'
+  subject.gid.should == '1000'
+  subject.personal_data.first.should == 'Keith Bennett'
+  subject.home_dir.should == '/home/kbennett'
+  subject.shell.should == '/usr/bin/zsh'
+end
+
+end
+
---
+++ spec/simple-unix-users/users_spec.rb
@@ -0,0 +1,26 @@
+require 'rspec'
+
+require_relative '../../lib/ruby-sysadmin-examples/users.rb'
+
+describe Users do
+
+subject { Users.from_passwd_file }
+
+it 'should load a list of users' do
+  subject.should be_a Array
+  subject.should_not be_empty
+end
+
+it 'should return a list of names for the names function' do
+  names = subject.names.sort
+  names.sort.uniq.size.should > 1
+end
+
+it 'should be able to get uids as numbers' do
+  uid_numbers = subject.uid_numbers
+  uid_numbers.should_not be_empty
+  uid_numbers.first.should be_a(Fixnum)
+end
+
+end
+
